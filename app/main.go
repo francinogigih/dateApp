9eecf9c51b0e91c6b8dddad17e925c7576cf8d6b
Francino Gigih <francino.gigih@visionplus.id>
2024-01-20 17:48:14 +0700

    register user service in main.go file

--- app/main.go
+++ app/main.go
@@ -5,12 +5,28 @@ import (
 	"dateApp/config"
 	"fmt"
 
+	pg "dateApp/pkg/common/modules/db"
+	userAPIHttp "dateApp/pkg/user/api/http"
+	userService "dateApp/pkg/user/business"
+	userRepository "dateApp/pkg/user/modules/repository"
+
 	"github.com/labstack/echo/v4"
 	mw "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
 )
 
+func newUserService(db *gorm.DB) userService.UserService {
+	userRepository := userRepository.NewPostgresDBRepository(db)
+	userService := userService.NewUserService(userRepository)
+	return userService
+}
+
 func main() {
+	db := pg.DatabaseConnection()
+
+	userService := newUserService(db)
+	userHandler := userAPIHttp.NewHandler(userService)
 	ctx := context.Background()
 	// define echo backend
 	e := echo.New()
@@ -20,6 +36,8 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.NoContent(200)
 	})
+
+	userAPIHttp.RegisterPath(e, userHandler)
 
 	address := fmt.Sprintf(":%d", config.GetConfig().AppPort)
 	if err := e.Start(address); err != nil {
