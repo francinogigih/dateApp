1dda5f0b5b5557c1c31d712d46f9b5042ea4fb36
Francino Gigih <francino.gigih@visionplus.id>
2024-01-23 04:16:50 +0700

    add login repository

--- pkg/user/core/repository.go
+++ pkg/user/core/repository.go
@@ -2,4 +2,5 @@ package user
 
 type UserRepository interface {
 	Create(userData *User) (int64, error)
+	Get(username, password string) (*User, error)
 }
--- pkg/user/modules/repository/user.go
+++ pkg/user/modules/repository/user.go
@@ -8,6 +8,7 @@ import (
 	appErr "dateApp/pkg/common/http"
 	user "dateApp/pkg/user/core"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
 )
 
@@ -48,12 +49,46 @@ func (repo *PostgresDBRepository) newUserData(userData *user.User) User {
 	}
 }
 
+func (repo *PostgresDBRepository) toCore(data User) *user.User {
+	return &user.User{
+		ID:         data.ID,
+		Username:   data.Username,
+		Password:   data.Password,
+		FullName:   data.FullName,
+		Email:      data.Email,
+		Phone:      data.Phone,
+		Gender:     data.Gender,
+		BirthPlace: data.BirthPlace,
+		BirthDate:  data.BirthDate,
+		Location:   data.Location,
+	}
+}
+
 func (repo *PostgresDBRepository) Create(data *user.User) (int64, error) {
 	userData := repo.newUserData(data)
-	result := repo.db.Save(&userData)
+	result := repo.db.Create(&userData)
 
 	if result.Error != nil {
+		if pgError := result.Error.(*pgconn.PgError); errors.Is(result.Error, pgError) {
+			if pgError.Code == "23505" {
+				return 0, errors.New(appErr.ErrAlreadyRegistered)
+			}
+			return 0, errors.New(appErr.ErrInternalServer)
+		}
 		return 0, errors.New(appErr.ErrInternalServer)
 	}
 	return userData.ID, nil
+}
+
+func (repo *PostgresDBRepository) Get(username, password string) (*user.User, error) {
+	var userData User
+	result := repo.db.Where("username = ?", username).Where("password = ?", password).First(&userData)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, errors.New(appErr.ErrInvalidUser)
+		}
+		return nil, errors.New(appErr.ErrInternalServer)
+	}
+
+	return repo.toCore(userData), nil
 }
